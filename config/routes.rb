9c6c8ad56e0fa4d8b5d613fb8438cca2607bca29
Dennis Neely <dennis.neely22@gmail.com>
2020-02-11 12:24:35 +0100

    show all, create, show one

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  get 'tasks', to: "tasks#index", as: :tasks
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/:id', to: 'tasks#show', as: :show
+  get 'new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
+  get 'edit', to: 'tasls#update', as: :edit
+
 end
