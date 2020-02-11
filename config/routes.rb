564c1c6797b6ff6aa62bfea375ef60616befa5de
Dennis Neely <dennis.neely22@gmail.com>
2020-02-11 13:26:19 +0100

    need to add delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index', as: :tasks
-  get 'tasks/:id', to: 'tasks#show', as: :show
-  get 'new', to: 'tasks#new', as: :new
-  post 'tasks', to: 'tasks#create'
-  get 'edit', to: 'tasls#update', as: :edit
 
+  get 'tasks',      to: 'tasks#index', as: :tasks
+  get 'tasks/:id',  to: 'tasks#show',  as: :task
+
+  get 'tasks/new',  to: 'tasks#new', as: :new
+  post 'tasks',     to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id',    to: 'tasks#update'
 end
