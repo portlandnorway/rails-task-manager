21f5d3c809502d3a29425a248cc9dbc68a8fe7dc
Dennis Neely <dennis.neely22@gmail.com>
2020-02-11 15:47:33 +0100

    all done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  resources :tasks
 
-  get 'tasks',      to: 'tasks#index', as: :tasks
-  get 'tasks/:id',  to: 'tasks#show',  as: :task
+  # get 'tasks',          to: 'tasks#index', as: :tasks
+  # get 'tasks/new',      to: 'tasks#new', as: :new
+  # get 'tasks/:id',      to: 'tasks#show',  as: :task
 
-  get 'tasks/new',  to: 'tasks#new', as: :new
-  post 'tasks',     to: 'tasks#create'
 
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id',    to: 'tasks#update'
+  # post 'tasks',         to: 'tasks#create'
+
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id',    to: 'tasks#update'
+
+  # delete "tasks/:id",   to: 'tasks#destroy'
 end
